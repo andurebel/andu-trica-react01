4a38caf7c4852c6b2bc149e884cff0dc54fdd78c
andurebel <andu.trica@gmail.com>
2021-10-07 19:46:25 +0300

    map is done

--- react-components/ex01/app.js
+++ react-components/ex01/app.js
@@ -1,7 +1,7 @@
 const person = {
   name: 'Dragos',
   surname: 'Iordache',
-  age: 32,
+  age: 34,
   petOwner: false,
   skills: [
     'html',
@@ -31,3 +31,59 @@ const person = {
     },
   ],
 };
+
+console.warn(
+  `folosind metoda map, returneaza si afiseaza in consola un array in care fiecare consoana e scrisa cu litera mare( vocalele nu)`,
+);
+
+let arr = [];
+
+arr = person.skills.map((skill) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const letterArray = skill.split('');
+
+  const mutatedLetterArray = [];
+
+  letterArray.forEach((letter) => {
+    if (vowels.includes(letter)) {
+      mutatedLetterArray.push(letter);
+    } else {
+      mutatedLetterArray.push(letter.toUpperCase());
+    }
+  });
+
+  return mutatedLetterArray.join('');
+});
+console.log(arr);
+
+console.warn(
+  `Folosing map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia "Ma numesc {name}{surname} si am {age} ani"`,
+);
+
+arr = person.friends.map(({ name, surname, age }) => {
+  return `Ma numesc ${name} ${surname} si am ${age} ani`;
+});
+console.log(arr);
+
+console.warn(`
+  folosind map pe arrayul friends, returneaza un array in care fiecare pozitie contine propozitia "diferenta de varsta dintre {friendName} si {age} este {diff}"
+`);
+
+arr = person.friends.map((friend) => {
+  const { name, age } = friend;
+
+  return `
+    Diferenta de varsta dintre ${name} si ${person.name} este${Math.abs(
+    person.age - age,
+  )} ani`;
+});
+console.log(arr);
+
+console.warn(
+  `Returneaza si afiseaza un array in care fiecare pozitie contine diferenta intre varsta persoanei si lungimea cuvantului de pe arrayul skill`,
+);
+
+arr = person.skills.map((skill) => {
+  return Math.abs(skill.length - person.age);
+});
+console.log(arr);
