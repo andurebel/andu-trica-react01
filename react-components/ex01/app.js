08eb956dda1478169ab06c7dc90fa2d73e83294b
andurebel <andu.trica@gmail.com>
2021-10-10 23:29:02 +0300

    map + reduce done

--- react-components/ex01/app.js
+++ react-components/ex01/app.js
@@ -87,3 +87,73 @@ arr = person.skills.map((skill) => {
   return Math.abs(skill.length - person.age);
 });
 console.log(arr);
+
+console.warn(`
+Folosind metoda map pe arrayul skills, returneaza un array care contine cuvintele cu prima si ultima litera mari.
+`);
+
+arr = person.skills.map((skill) => {
+  const [firstLetter, ...letters] = skill;
+
+  const firstUp = firstLetter.toUpperCase();
+  const finalUp = letters[letters.length - 1].toUpperCase();
+  const middle = letters.slice(0, skill.length - 2).join('');
+
+  const word = firstUp + middle + finalUp;
+  return word;
+});
+console.log(arr);
+
+console.warn(`Folosind metoda map pe arrayul skills, returneaza un array care contine cuvintele inversate (exemplu: lmth)
+`);
+
+arr = person.skills.map((skill) => {
+  const backward = skill.split('');
+  const reversed = backward.reverse(0);
+  return reversed.join('');
+});
+
+console.log(arr);
+
+console.warn(`Folosind metoda map pe arrayul friends, returneaza un array care sa contina propozitiile
+“{friendName} are {age} ani.”`);
+
+arr = person.friends.map(({ name, age }) => {
+  return `${name} are ${age} ani`;
+});
+
+console.log(arr);
+
+console.warn(
+  `Folosind metoda map pe arrayul friends, returneaza un array care contine numele inversat al prietenilor pe fiecare pozitie (exemplu: Stevenson Steven)`,
+);
+
+arr = person.friends.map(({ name }) => {
+  const nameSplit = name.split('');
+  const finalName = nameSplit.reverse().join('');
+  return finalName;
+});
+
+console.log(arr);
+
+console.warn(
+  `Folosind metoda map pe arrayul friends, returneaza un array care contine pe fiecare pozitie diferenta dintre lungimea totala al numelui complet (fara spatii) si varsta prietenului de pe iteratie`,
+);
+
+arr = person.friends.map(({ name, surname, age }) => {
+  const length = name.length - surname.length;
+
+  return Math.abs(length - age);
+});
+
+console.log(arr);
+
+console.warn(
+  `Folosind metoda map pe arrayul skills returneaza un array care contine diferenta dintre lungimea fiecarui skill si varsta prietenului`,
+);
+
+arr = person.skills.map((skill, age) => {
+  return Math.abs(skill.length - age);
+});
+
+console.log(arr);
--- react-components/ex02/app.js
+++ react-components/ex02/app.js
@@ -96,3 +96,85 @@ let filteredSkills = person.skills.reduce((filteredSkills, skill) => {
 }, []);
 
 console.log(filteredSkills);
+
+console.warn(`Folosind reduce afiseaza propozitia: "Prietenii mei se numesc xxx yyy, xxx yyy, xxx yyy."
+`);
+
+message = person.friends.reduce(
+  (message, { name, surname }, index, friends) => {
+    let punctuation = index === friends.length - 1 ? '.' : ', ';
+
+    return `${message}${name} ${surname} ${punctuation}`;
+  },
+  'Prietenii mei se numesc',
+);
+
+console.log(message);
+
+console.warn(
+  `Folosind reduce, afiseaza numarul total de ani pe care il au persoanele din arrayul friends, doar daca varsta este mai mare sau egala cu 30 de ani.`,
+);
+
+message = person.friends.reduce((sum, friend) => {
+  let { age } = friend;
+  if (age >= 30) {
+    return sum + age;
+  } else {
+    return sum;
+  }
+}, 0);
+
+console.log(message);
+
+console.warn(`
+Folosind reduce, afiseaza suma anilor de nastere a persoanelor.`);
+
+message = person.friends.reduce((message, friend) => {
+  const currentYear = 2021;
+  const { age } = friend;
+  const birthday = currentYear - age;
+  return `${message} ${birthday}`;
+}, 'Suma anilor de nastere este ');
+
+// nu stiu sa fac :(
+
+console.log(message);
+
+console.warn(`
+Afiseaza fraza: "Intre Dragos si Larry este o diferenta de xx ani. Intre Dragos si Steven... ", doar daca varsta prietenului este impara. `);
+
+message = person.friends.reduce((message, friend, friends) => {
+  if (friend.age % 2 !== 0) {
+    return `${message} ${friend.name} este o diferenta de ${
+      person.age - friend.age
+    } ani`;
+  } else {
+    return message;
+  }
+}, `Intre ${person.name} si `);
+console.log(message);
+
+console.warn(
+  `Folosind obiectul person si reduce, afiseaza in consola un string care contine skillurile persoanei, separate prin virgula`,
+);
+
+message = person.skills.reduce((accumulator, value, index, friends) => {
+  return `${accumulator}${index === friends.skills - 1 ? '.' : ','}${value}`;
+}, []);
+
+console.log(message);
+
+console.warn(
+  `Folosind reduce afiseaza propozitia: "Numele de familie ale prietenilor mei sunt: xxx, xxx , xxx."`,
+);
+
+message = person.friends.reduce((message, friends, index) => {
+  return `${message} ${friends.surname}${index === friends.length ? '.' : ','}`;
+}, 'Numele de familie ale prietenilor mei sunt');
+
+console.log(message);
+
+console.warn(` Folosind reduce, afiseaza suma anilor  persoanelor. `);
+
+message = person.friends.reduce((sum, { age }) => sum + age, person.age);
+console.log(message);
