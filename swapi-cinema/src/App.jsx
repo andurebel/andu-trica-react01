267c5d3caf52f57c870cee801a1b121327aa2c71
andurebel <andu.trica@gmail.com>
2021-10-12 21:00:10 +0300

    synced with dragos

--- swapi-cinema/src/App.jsx
+++ swapi-cinema/src/App.jsx
@@ -1,11 +1,19 @@
 import { Component, Fragment } from 'react';
+import Film from './components/Film';
+import Films from './components/Films';
+import PurchaseFilm from './components/PurchaseFilm';
 import Search from './components/Search';
 
 const baseUrl = 'https://swapi.dev/api/films';
 
 class App extends Component {
   state = {
+    busy: true,
     films: [],
+    errorMessage: '',
+    hasSearchResults: false,
+    selectedFilm: null,
+    purchasing: false,
   };
 
   getFilms() {
@@ -13,9 +21,13 @@ class App extends Component {
       busy: true,
     });
 
-    //promise chaining
-    fetch(baseUrl)
+    // promise chaining
+    return fetch(baseUrl)
       .then((response) => {
+        if (response.status === 404) {
+          throw new Error('404');
+        }
+
         return response.json();
       })
       .then(({ results }) => {
@@ -23,20 +35,89 @@ class App extends Component {
           films: results,
           busy: false,
         });
+      })
+      .catch((_) => {
+        this.setState({
+          errorMessage: 'An error has occured.',
+          busy: false,
+        });
       });
   }
 
-  renderFilms() {
-    return this.state.films.map((film) => {
-      return <p key={film.episode_id}>{film.title}</p>;
+  clearSearchResults() {
+    this.getFilms().then(() => {
+      this.setState({
+        hasSearchResults: false,
+      });
     });
+  }
+
+  renderFilms() {
+    return (
+      <>
+        <h2>Available films</h2>
+
+        <Films
+          films={this.state.films}
+          selectFilm={(film) => {
+            this.setState({
+              selectedFilm: film,
+            });
+          }}
+          purchaseFilm={(film) => {
+            this.setState({
+              selectedFilm: film,
+              purchasing: true,
+            });
+          }}
+        ></Films>
+        {this.state.hasSearchResults ? (
+          <button
+            className="btn btn-warning text-white"
+            title="See all movies"
+            type="button"
+            onClick={() => {
+              this.clearSearchResults();
+            }}
+          >
+            See all movies
+          </button>
+        ) : (
+          <></>
+        )}
+      </>
+    );
+  }
+
+  renderFilm() {
+    return (
+      <Film
+        film={this.state.selectedFilm}
+        deselectFilm={() => {
+          this.setState({
+            selectedFilm: null,
+          });
+        }}
+      ></Film>
+    );
   }
 
   renderMainScreen() {
     if (this.state.busy === true) {
-      return <>Loading...</>;
+      return <>... loading</>;
     }
-    return this.renderFilms();
+
+    if (this.state.busy === false && this.state.errorMessage.length > 0) {
+      return <>{this.state.errorMessage}</>;
+    }
+
+    if (this.state.purchasing === true) {
+      return <PurchaseFilm film={this.state.selectedFilm}></PurchaseFilm>;
+    }
+
+    return this.state.selectedFilm !== null
+      ? this.renderFilm()
+      : this.renderFilms();
   }
 
   componentDidMount() {
@@ -49,8 +130,16 @@ class App extends Component {
         <header className="navbar-expand-md navbar-dark fixed-top bg-dark">
           <nav className="container d-flex justify-content-between">
             <h1 className="display-6 text-warning">Swapi Cinema</h1>
-
-            <Search></Search>
+            <Search
+              onSearchResults={(films) => {
+                this.setState({
+                  films,
+                  hasSearchResults: true,
+                  selectedFilm: null,
+                });
+              }}
+              placeholder="Choose a SW movie"
+            ></Search>
           </nav>
         </header>
 
