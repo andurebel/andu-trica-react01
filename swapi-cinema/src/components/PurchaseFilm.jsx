7b3639779cfdea3a28d086d3daff8bd096555652
andurebel <andu.trica@gmail.com>
2021-10-12 21:59:55 +0300

    app done

--- swapi-cinema/src/components/PurchaseFilm.jsx
+++ swapi-cinema/src/components/PurchaseFilm.jsx
@@ -6,23 +6,103 @@ const columns = 10;
 const ticketPrice = 3;
 
 class PurchaseFilm extends Component {
+  state = {
+    selectedSeats: new Set(),
+  };
+
+  toggleSeatSelect(seatKey) {
+    const selectedSeats = new Set(this.state.selectedSeats);
+
+    if (selectedSeats.has(seatKey)) {
+      selectedSeats.delete(seatKey);
+    } else {
+      selectedSeats.add(seatKey);
+    }
+
+    this.setState({
+      selectedSeats,
+    });
+  }
+
   renderSeats() {
     const seats = [];
 
     for (let i = 0; i < rows.length; i++) {
-      for (let j = 1; j <= 10; j++) {
-        // A1 B4 C8
+      for (let j = 1; j <= columns; j++) {
+        // A-1 B-4 C-8
         const seatKey = `${rows[i]}-${j}`;
-        const cssClasses = `btn btn-sm py-2 btn-outline-dark`;
+        const seatSelected = this.state.selectedSeats.has(seatKey);
+        const cssClasses = `btn btn-sm py-2 ${
+          seatSelected ? 'btn-warning' : 'btn-outline-dark'
+        }`;
 
-        seats.push(<button className={cssClasses} key={seatKey}></button>);
+        seats.push(
+          <button
+            className={cssClasses}
+            key={seatKey}
+            type="button"
+            title={seatKey}
+            onClick={() => {
+              this.toggleSeatSelect(seatKey);
+            }}
+          ></button>,
+        );
       }
     }
 
     return seats;
   }
 
+  purchaseSeats = () => {
+    const total = this.state.selectedSeats.size * ticketPrice;
+    const orderObject = {
+      seats: [...this.state.selectedSeats],
+      total,
+      filmName: this.props.film.title,
+      filmId: this.props.film.episode_id,
+    };
+
+    console.log(JSON.stringify(orderObject));
+  };
+
+  renderTotalLines() {
+    return Array.from(this.state.selectedSeats).map((selectedSeatKey) => {
+      return (
+        <tr key={selectedSeatKey}>
+          <td>Seat: {selectedSeatKey}</td>
+          <td>USD{ticketPrice}</td>
+        </tr>
+      );
+    });
+  }
+
+  renderTotals() {
+    const selectedSeats = this.state.selectedSeats.size;
+
+    if (selectedSeats <= 0) {
+      return null;
+    }
+
+    const total = ticketPrice * selectedSeats;
+
+    return (
+      <table className="table table-stripped table-dark">
+        <tbody>
+          {this.renderTotalLines()}
+
+          <tr>
+            <td className="fw-bold">Totals:</td>
+            <td>USD{total}</td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  }
+
   render() {
+    const selectedTicketsCount = this.state.selectedSeats.size;
+    const buttonTitle = `Buy tickets for ${this.props.film.title}`;
+
     return (
       <section className="row">
         <header className="col-12 d-flex mb-4 justify-content-between align-items-center">
@@ -37,7 +117,19 @@ class PurchaseFilm extends Component {
           </div>
         </div>
 
+        <div className="col-12">{this.renderTotals()}</div>
+
         <div className="col-12">
+          {selectedTicketsCount > 0 ? (
+            <button
+              className="btn btn-warning text-white"
+              title={buttonTitle}
+              onClick={this.purchaseSeats}
+            >
+              Buy
+            </button>
+          ) : null}
+
           <button
             className="btn btn-outline-light d-inline float-end"
             title="Back"
