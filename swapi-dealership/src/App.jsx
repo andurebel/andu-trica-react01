e8a319cf574bc91fa2c5b81d069e2e56f2119af6
andurebel <andu.trica@gmail.com>
2021-10-14 22:10:24 +0300

    starting swapi vehicles

---
+++ swapi-dealership/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <header className="navbar-dark fixed-top bg-dark border-bottom border-warning">
+        <nav className="container d-flex justify-content-between">
+          <h1 className="display-6 text-warning">Swapi vehicles</h1>
+        </nav>
+      </header>
+      <main className="container mb-4">Content</main>
+      <footer className="container mb-4">Footer</footer>
+    </>
+  );
+};
+
+export default App;
