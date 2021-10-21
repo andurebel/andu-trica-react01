8b5846af3802cec2da7d893eb7d22943095ecab6
andurebel <andu.trica@gmail.com>
2021-10-21 22:50:21 +0300

    1050

--- swapi-dealership/src/components/ProductDetails.jsx
+++ swapi-dealership/src/components/ProductDetails.jsx
@@ -1,21 +1,23 @@
 export const ProductDetails = ({ product }) => {
-const renderTableRows = () => {
-  const entries = Object.entries(product);
-  entries.map(([feature, value]) => {
-    return (
-      <tr>
-        <td>{feature}</td>
-        <td>{value}</td>
-      </tr>
-    );
-  });
-};
+  const renderTableRows = () => {
+    const entries = Object.entries(product);
 
-return (
-  <table className="table table-dark">
-    <tbody>{renderTableRows()}</tbody>
-  </table>
-);
+    return entries.map(([feature, value]) => {
+      return (
+        <tr key={feature}>
+          <td>{feature}</td>
+
+          <td>{value}</td>
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <table className="table table-dark">
+      <tbody>{renderTableRows()}</tbody>
+    </table>
+  );
 };
 
 export default ProductDetails;
