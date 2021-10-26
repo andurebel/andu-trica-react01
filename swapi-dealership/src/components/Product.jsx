b5302dd4ba571622d725a791ee3441eae82b35bd
andurebel <andu.trica@gmail.com>
2021-10-26 20:03:35 +0300

    removed MetaImage

--- swapi-dealership/src/components/Product.jsx
+++ swapi-dealership/src/components/Product.jsx
@@ -1,6 +1,6 @@
 import { useContext, useMemo } from 'react';
 import { AppContext } from '../contexts/AppContext';
-import MetaImage from './../legacy/MetaImage';
+//import MetaImage from './../legacy/MetaImage';
 import { ProductDetails } from './ProductDetails';
 
 export const Product = () => {
