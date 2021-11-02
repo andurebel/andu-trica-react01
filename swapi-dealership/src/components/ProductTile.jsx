75753b0aabf483648717989a4e716c72a1ba459e
andurebel <andu.trica@gmail.com>
2021-11-02 17:18:26 +0200

    added Emoji instead of MetaImage

--- swapi-dealership/src/components/ProductTile.jsx
+++ swapi-dealership/src/components/ProductTile.jsx
@@ -22,6 +22,15 @@ export const ProductTile = ({ product, productInCart }) => {
     <article className="col-6 col-md-3 mb-6 d-flex flex-column">
       <header className="flex-grow-1 text-center mb-4">
         <h5 className="text-warning text-left">{name}</h5>
+        <div className="img-responsive bg-secondary">
+          <p className="py-5">I have no image, sorry :(</p>
+          <img
+            height="200"
+            width="200"
+            src="https://i.pinimg.com/originals/da/8c/21/da8c2163061be58d770f6f9af78258e8.png"
+            alt="sad"
+          />
+        </div>
         <h6>({model})</h6>
 
         {/* <MetaImage term={name}></MetaImage> */}
