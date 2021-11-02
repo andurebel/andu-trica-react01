4ad1c29a2b8d95af05d23f5c341c15092ad3c6ba
andurebel <andu.trica@gmail.com>
2021-11-02 20:02:36 +0200

    boilerplate done

---
+++ generic-game/src/App.jsx
@@ -0,0 +1,13 @@
+import { Footer, Header } from "./components/common";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <main>app goes here</main>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
