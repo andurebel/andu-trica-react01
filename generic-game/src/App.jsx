ae981a86ac21e4557912dff638aabd2b4e43d3ee
andurebel <andu.trica@gmail.com>
2021-11-04 22:25:04 +0200

    auth done

--- generic-game/src/App.jsx
+++ generic-game/src/App.jsx
@@ -1,25 +1,26 @@
-import { Footer, Header } from "./components/common";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { clickClicker, decrementClicker } from "./actions/creators/ui";
+import { initializeGoogleAuth } from "./api";
+import { Footer, Header } from "./components/common";
+// import { STH, STH } from './actions/types/ui';
+// import {dispatchState} from './actions/creators/ui';
+
+// async
+initializeGoogleAuth();
 
 export const App = () => {
   const clicker = useSelector((state) => {
     const { ui } = state;
+
     return ui.clicker;
   });
   const dispatch = useDispatch();
+
   return (
     <>
-      <Header />
+      <Header></Header>
       <main>
-        <div>Value is : {clicker}</div>
-        <button
-          onClick={() => {
-            dispatch(clickClicker());
-          }}
-        >
-          Increment
-        </button>
+        <div>Value is: {clicker}</div>
         <button
           onClick={() => {
             dispatch(decrementClicker());
@@ -27,13 +28,20 @@ export const App = () => {
         >
           Decrement
         </button>
+        <button
+          onClick={() => {
+            dispatch(clickClicker());
+          }}
+        >
+          Test
+        </button>
       </main>
-      <Footer />
+      <Footer></Footer>
     </>
   );
 };
 
 export default App;
 
-//actions ->{type:'',payload:{}} /types /creators
-//reducers 
+// actions -> {type: '', payload: {}}  /types /creators
+// reducers
