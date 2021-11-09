a6c976cec89b02f735b18bd7303ed0f34bf7af40
andurebel <andu.trica@gmail.com>
2021-11-09 23:13:31 +0200

    HomePage done

--- generic-game/src/components/common/Header.jsx
+++ generic-game/src/components/common/Header.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { SiLetterboxd } from "react-icons/si";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-  return <div>header</div>;
+  const renderUserControls = () => {
+    const authenticated = true;
+
+    if (authenticated) {
+      return "user is logged in";
+    } else {
+      return "user is not logged in";
+    }
+  };
+
+  return (
+    <>
+      <header className="shadow p-4">
+        <div className="container mx-auto flex justify-between items-center">
+          <header>
+            <h1 className="uppercase text-lg font-bold">
+              <Link to="/" title="Go home" className="flex items-center">
+                <SiLetterboxd className="mr-2"></SiLetterboxd>
+              </Link>
+            </h1>
+          </header>
+          {renderUserControls()}
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;
