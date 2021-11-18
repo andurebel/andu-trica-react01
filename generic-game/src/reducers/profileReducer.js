e83db9cfb47c659eb4e93d2d83d6e1891021eda6
andurebel <andu.trica@gmail.com>
2021-11-18 19:48:39 +0200

    profile reducer/stats done

---
+++ generic-game/src/reducers/profileReducer.js
@@ -0,0 +1,24 @@
+import { PROFILE_SET_STATS } from "../actions/types/profile";
+
+const initialState = {
+  stats: {
+    gamesWon: 0,
+    gamesLost: 0,
+    gamesPlayed: 0,
+  },
+};
+
+export const profileReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case PROFILE_SET_STATS:
+      return {
+        ...state,
+        stats: payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default profileReducer;
