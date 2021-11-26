ee5a808f826797fb16305c55a00f751cdc337f65
andurebel <andu.trica@gmail.com>
2021-11-26 11:58:43 +0200

    changed timeout

--- swapi-cinema/src/components/MetaImage.jsx
+++ swapi-cinema/src/components/MetaImage.jsx
@@ -1,25 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const baseUrl =
-  'https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI';
+  "https://contextualwebsearch-websearch-v1.p.rapidapi.com/api/Search/ImageSearchAPI";
 const apiKey = process.env.REACT_APP_WEB_SEARCH_KEY;
-const host = 'contextualwebsearch-websearch-v1.p.rapidapi.com';
+const host = "contextualwebsearch-websearch-v1.p.rapidapi.com";
 
 export const MetaImage = ({ term }) => {
   const [busy, setBusy] = useState(true);
-  const [imageUrl, setImageUrl] = useState('');
+  const [imageUrl, setImageUrl] = useState("");
 
   useEffect(() => {
     const refinedSearchTerm = encodeURIComponent(`star wars ${term}`);
-    const random = Math.floor(Math.random() * 2000) + 1;
+    const random = Math.floor(Math.random() * 1000) + 1;
 
     setTimeout(() => {
       fetch(
         `${baseUrl}?q=${refinedSearchTerm}&pageNumber=1&pageSize=1&autoCorrect=true`,
         {
           headers: {
-            'x-rapidapi-key': apiKey,
-            'x-rapidapi-host': host,
+            "x-rapidapi-key": apiKey,
+            "x-rapidapi-host": host,
             useQueryString: true,
           },
         },
@@ -39,7 +39,7 @@ export const MetaImage = ({ term }) => {
   return (
     <>
       {busy === true ? (
-        '...loading'
+        "...loading"
       ) : (
         <img className="img-fluid" src={imageUrl} alt={term}></img>
       )}
